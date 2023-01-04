9e2b83a2bf5929ef88d1a886acfb103138cdd77e
ابو رضا <46461177+hamid-kano@users.noreply.github.com>
2023-01-04 12:05:49 +0300

    Create test.c

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+   int sz;
+   printf("Enter the size of array:");
+   scanf("%d",&sz);
+   int randArray[sz][sz],i,j;
+   for(i=0;i<sz;i++)
+   {
+      for(j=0;j<sz;j++)
+      {
+      randArray[i][j]=rand()%20;
+      }
+   }//Generate number between 0 to 99
+   printf("\nElements of the array:");
+   printf("\n");
+   for(i=0;i<sz;i++)
+   {
+       for(j=0;j<sz;j++)
+       {
+         printf(" %d", randArray[i][j]);
+       }
+         printf("\n");
+   }
+   return 0;
+}
